3a030f52f3d0ffb20597233c9870e3c91834397c
Athiley Luciano <athileyluciano@gmail.com>
2021-01-05 15:41:43 +0100

    review interpolation

--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
@@ -1,8 +1,10 @@
 require_relative "compute_name"
 
-# TODO: ask for the first name
-# TODO: ask for the middle name
-# TODO: ask for the last name
-# TODO: Print a greeting to the user with the full name concatenated,
-#       something like "Hello, Boris Paillard"
+puts "What's your first name?"
+first_name = gets.chomp
+puts "What's your second name?"
+middle_name = gets.chomp
+puts "What's your last name?"
+last_name = gets.chomp
 
+puts "Hello #{first_name} #{middle_name} #{last_name}"
